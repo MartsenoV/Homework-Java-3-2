2a87cfd7c0ad662e9b86e39866c93dc970829c4f
Martsenov <lera-marcenovich@mail.ru>
2024-11-05 20:05:21 +0300

    Initial commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
-public class BmiService {
-    public float calculate(float weight, float height) {
-        float bmi = weight / (height * height);
-        return (int) bmi;
-
+public class BonusMilesService {
+    public int calculate(int ticketPrice) {
+        int totalMiles = ticketPrice / 20;
+        return totalMiles;
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        BmiService service = new BmiService();
-        float weight = 98f;
-        float height = 1.87f;
-        float bmi = service.calculate(weight, height);
-        System.out.println("Индекс массы тела = " + bmi);
+        BonusMilesService service = new BonusMilesService();
+        int ticketPrice = 17342;
+        int totalMiles = service.calculate(ticketPrice); // должно получиться 500
+        System.out.println(totalMiles);
     }
 }
